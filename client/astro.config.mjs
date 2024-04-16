a746e3889cef203b2d33eee80f32f2810ffb29a9
gw31415 <git@amas.dev>
2024-04-16 09:44:32 +0900

    add astro-compress

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig ***REMOVED*** from "astro/config";
-
 import solidJs from "@astrojs/solid-js";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()],
+  integrations: [solidJs(), compress()]
 ***REMOVED***);
