6088f8afd1810b872c64d52e5a14b6582479ef84
gw31415 <git@amas.dev>
2024-04-16 09:59:40 +0900

    disable astro-compress on JavaScript

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig ***REMOVED*** from "astro/config";
 import solidJs from "@astrojs/solid-js";
 
-import compress from "astro-compress";
+import Default from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), compress()]
+  integrations: [solidJs(), Default({
+    JavaScript: false,
+  ***REMOVED***)]
 ***REMOVED***);
