2f62cbe1125beb1df23c6fde2eb7e04d5871f63d
gw31415 <git@amas.dev>
2024-04-18 10:58:58 +0900

    temporal migration

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,12 +1,16 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20240410_000001_create_table;
+mod m20240418_000002_create_mstdn_token_table;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20240410_000001_create_table::Migration)]
+        vec![
+            Box::new(m20240410_000001_create_table::Migration),
+            Box::new(m20240418_000002_create_mstdn_token_table::Migration),
+        ]
     }
 }
