cbe2748046f78f8d0ebc0428fe46d3950a7af21c
gw31415 <git@amas.dev>
2024-04-21 11:13:02 +0900

    new styling system

---
+++ client/src/typing.ts
@@ -0,0 +1,13 @@
+export function loadTyping(el: HTMLDivElement) {
+	if (!el.classList.contains("typing")) el.classList.add("typing");
+	const len = (el.textContent ?? "").length + 1;
+	el.style.animation = `typing ${Math.min(
+		1,
+		0.1 * len,
+	)}s steps(${len}), blink .4s step-end infinite alternate`;
+	el.style.width = `${len}ch`;
+	el.addEventListener("animationend", () => {
+		el.style.animation = "none";
+		el.style.borderRight = "none";
+	});
+}
